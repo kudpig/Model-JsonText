1d001baa5dba37887c855dc70d1edf5be11c5be6
kudpig <shin.kudo.aa@gmail.com>
2021-02-26 15:44:45 +0900

    Initial Commit

---
+++ Model-JsonText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Model-JsonText
+//
+//  Created by Shinichiro Kudo on 2021/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
